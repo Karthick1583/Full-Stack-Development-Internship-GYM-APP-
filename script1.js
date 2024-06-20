5874b400d76fd47077951d12fe517793c68ee84a
Saravanak Karthick <sara.karth158@gmail.com>
2024-06-20 13:02:43 +0530

    added templates and static files for the new gym app

--- script1.js
+++
@@ -1,3 +0,0 @@
-function myFunction1(name){
-    document.getElementById("demo").innerHTML = "paragraph 1" + name;
-}
--- script2.js
+++
@@ -1,4 +0,0 @@
-function myFunction2(){
-    document.getElementById("demo").innerHTML = "paragraph 2";
-    document.getElementById("demo").style.fontSize = "75px";
-}
